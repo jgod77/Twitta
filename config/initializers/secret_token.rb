28f73e2a8425a0bb11a64aaecf78edc45632bbf7
Your Name <you@example.com>
2012-03-12 18:00:42 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twitta::Application.config.secret_token = 'f36805857102da93bde3509899486eee8a9612b4db34f0cae82541cd68d91e2242868a46aa0f2d7fb8e9eedaef72ce1fbdb84b0f53823b1554bca45cad4c92f0'
